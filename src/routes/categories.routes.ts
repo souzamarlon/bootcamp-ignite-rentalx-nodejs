65fc8f1b282b51a6c3ff3ada5964b58c757167eb
souzamarlon <marlon.bobi@hotmail.com>
2021-03-31 03:39:35 -0300

    Added the function list to return all the categories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -4,12 +4,19 @@ import { CategoriesRepository } from '../repositories/CategoriesRepository';
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
+
 categoriesRoutes.post('/', (req, res) => {
   const { name, description } = req.body;
 
   categoriesRepository.create({ name, description });
 
   return res.status(201).send();
+});
+
+categoriesRoutes.get('/', (req, res) => {
+  const all = categoriesRepository.list();
+
+  return res.json(all);
 });
 
 export { categoriesRoutes };
