df286c3c724ef248d3a60b8aa72ceff857461d22
souzamarlon <marlon.bobi@hotmail.com>
2021-03-31 02:35:06 -0300

    Added debug to the dev script

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,16 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   return res.json('Hello');
+});
+
+app.post('/courses', (req, res) => {
+  const { name } = req.body;
+
+  return res.json({ name });
 });
 
 app.listen(3333);
