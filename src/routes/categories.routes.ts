44217277a9d1159cf2955749b85bf5890cace77a
souzamarlon <marlon.bobi@hotmail.com>
2021-03-31 02:56:16 -0300

    Added the uuid library

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,16 +1,21 @@
 import { Router } from 'express';
+import { v4 as uuidV4 } from 'uuid';
 
 const categoriesRoutes = Router();
 
 const categories = [];
 
-categoriesRoutes.post('/categories', (req, res) => {
+categoriesRoutes.post('/', (req, res) => {
   const { name, description } = req.body;
 
-  categories.push({
+  const category = {
     name,
     description,
-  });
+    id: uuidV4(),
+    created_at: new Date(),
+  };
+
+  categories.push(category);
 
   return res.status(201).json(categories);
 });
