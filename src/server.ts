1c17848aa0415c02cabf40e8041c916329f5bd74
souzamarlon <marlon.bobi@hotmail.com>
2021-03-31 02:46:50 -0300

    We created the categorie route

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,10 @@
 import express from 'express';
 
+import { categoriesRoutes } from './routes/categories.routes';
+
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  return res.json('Hello');
-});
-
-app.post('/courses', (req, res) => {
-  const { name } = req.body;
-
-  return res.json({ name });
-});
-
+app.use(categoriesRoutes);
 app.listen(3333);
